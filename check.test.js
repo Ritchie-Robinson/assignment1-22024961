ec15b395cf190b4c74804828f4af78ab22fdd8a1
Ritchie-Robinson <78422233+Ritchie-Robinson@users.noreply.github.com>
2024-04-02 15:16:50 +0100

    Test written for key not found
    Key not found test

--- check.test.js
+++ check.test.js
@@ -2,12 +2,19 @@ const check = require('./check');
 
 describe('Answer check function', () => {
 
+    //if key found and right answer
     test('if correct key and top and left values correct, equal true', () => {
         expect(check('balances', 240, 0)).toBe(true);
     })
 
+    //if key found but wrong answer
     test('if questionKey exists, equal true', () => {
         expect(check('balances', 100, 100)).toBe(false);
+    })
+
+    //if key not found
+    test('if questionKey not found', () => {
+        expect(check('eevevw', 100, 100)).toBe('no such key');
     })
   
   });
