0d5d680cec93d0eecfe7016ac8596d2fccd8879e
Ritchie-Robinson <78422233+Ritchie-Robinson@users.noreply.github.com>
2024-04-02 15:05:09 +0100

    Check URL based on key instead
    Iterate through the data to check URL matches key instead of title, as key or title will not be known

--- data.test.js
+++ data.test.js
@@ -8,16 +8,33 @@ describe('Data Load', () => {
       expect(data).toBeDefined();
     });
 
-    // does end of URL path match title of the data?
-    test('URL path should match title', () => {
+    //check URL matches a key present in the data
+    test('URL path should a key present in the data', () => {
       const url = 'https://i-want-to-study-engineering.org/q/balances'; //TODO: in reality, this url would be passed in not hardcoded
       const lastPartOfUrl = url.substring(url.lastIndexOf('/') + 1);
-      const title = data.balances.questions.title;
-      expect(lastPartOfUrl).toBe(title);
+      
+      //iterate over keys in data object
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+    
+          // does last part of URL match key
+          if (lastPartOfUrl === key) {
+            expect(lastPartOfUrl).toBe(key);
+            return; //exit once match is found
+          }
+        }
+      }
+      
+      // If no match, fail test
+      fail('No matching questionKey found for URL');
     });
 
 
-  
+    // check there is a full question
+    test('There should be a full question to display', () => {
+      expect(data.balances.questions.fullquestion.question).toBeDefined();
+    });
+    
   
 });
 
