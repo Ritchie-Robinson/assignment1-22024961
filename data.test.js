e717de3e3b7fd7397236d17c857410a6057b07ea
Ritchie-Robinson <78422233+Ritchie-Robinson@users.noreply.github.com>
2024-04-02 12:24:13 +0100

    Check URL matches data loaded
    Check URL matches the data loaded, in this instance 'balances'. If this was a real application, this would be future development.

--- data.test.js
+++ data.test.js
@@ -7,6 +7,17 @@ describe('Data Load', () => {
     test('Data should be loaded', () => {
       expect(data).toBeDefined();
     });
+
+    // does end of URL path match title of the data?
+    test('URL path should match title', () => {
+      const url = 'https://i-want-to-study-engineering.org/q/balances'; //TODO: in reality, this url would be passed in not hardcoded
+      const lastPartOfUrl = url.substring(url.lastIndexOf('/') + 1);
+      const title = data.balances.questions.title;
+      expect(lastPartOfUrl).toBe(title);
+    });
+
+
+  
   
 });
 
