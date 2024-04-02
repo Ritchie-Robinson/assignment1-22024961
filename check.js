0823a23f91a72522b9ac45911d478c9dd3d6947f
Ritchie-Robinson <78422233+Ritchie-Robinson@users.noreply.github.com>
2024-04-02 15:18:57 +0100

    check function amended to prevent undefined
    As a result of the key not found test, function amended to prevent undefined error with an initial check of whether the key exists if(data.hasOwnProperty(questionKey))

--- check.js
+++ check.js
@@ -1,18 +1,24 @@
 const data = require('./data');
 
 function check(questionKey, userTop, userLeft) {
-    //get correct answer from data structure
-    const correctAnswer = data[questionKey].questions.fullquestion.answer.find(answer => answer.correct === true);
-  
-    if (correctAnswer) {
+
+    //first check if key exists to avoid undefined error
+    if (data.hasOwnProperty(questionKey)) {
+
+        //get correct answer from data structure
+        const correctAnswer = data[questionKey].questions.fullquestion.answer.find(answer => answer.correct === true);
+
         if (userTop === correctAnswer.top && userLeft === correctAnswer.left) {
             // return true if user's answer is correct
             return correctAnswer.correct;
         } else {
             return false;
         }
+
+    } else {
+        return 'no such key';
     }
 
-  }
+}
 
-  module.exports = check;
+module.exports = check;
