5a5b550efc969261bc5b9c9ded3c693e33194eab
Ritchie-Robinson <78422233+Ritchie-Robinson@users.noreply.github.com>
2024-04-09 12:20:51 +0100

    Just random console logging throughout development
    Just random console logging in the index.js file used throughout development of tests, left in but commented out for record.

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const data = require('./data');
+const checkUserAnswer = require('./check');
 
-console.log(data);
+// const correctAnswer = data['balances'].questions.fullquestion.answer.find(answer => answer.correct === true);
+
+// console.log(correctAnswer);
+// console.log(correctAnswer.top);
+// console.log(correctAnswer.left);
+
+// const answer = checkUserAnswer('balances', 240, 0);
+// console.log(answer);
+
+// console.log(data);
+
+// const answers = data?.balances.questions?.fullquestion?.answer;
+
+// console.log('answers array', answers);
+
+// const isCorrect = answers.some(answer => answer.correct === true);
+
+// console.log('true or false?', isCorrect);
 
